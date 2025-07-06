e0a7b6ddb93d75535f9a6652a35b2f6da3dab7ac
Lukas Scheucher <scheuclu@gmail.com>
2025-07-06 16:04:53 +0100

    Fixed type errors.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logfire
 from dotenv import load_dotenv
 from pydantic_ai import Agent, Tool
+from pydantic_ai.agent import AgentRunResult
 
 from warrenbotfett.api.instruments import list_instruments
 from warrenbotfett.api.orders import place_buy_order, place_sell_order
@@ -26,7 +27,6 @@ agent = Agent(
     model_settings=GoogleModelSettings(
         google_thinking_config={"include_thoughts": True}
     ),
-    deps_type=str,
     system_prompt=(
         "You are a stock investor that does thorough resaerch before any investments."
         "You have access to trading212 via a bunch of tools."
@@ -47,7 +47,7 @@ agent = Agent(
     output_type=BotSummary,
 )
 
-run_result: AgentRunResult = agent.run_sync(  # type: ignore
+run_result: AgentRunResult[BotSummary] = agent.run_sync(
     user_prompt="Analyze the market and the current holdings and then make a descion whether to change anyhting. You have all the tools avaialble, so you can trade. Be desicive. Summarize what you did at the end. Actually, make at least one trade."
 )
 success = store_summary(summary=run_result.output)
